ab7d31accf06a47e6944e03e5b5161f511f56c00
Maksym M <maximus@MacBook-Pro.local>
2024-09-14 17:41:15 -0400

    amplify sync

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -6,6 +6,17 @@ export type AmplifyDependentResourcesAttributes = {
       "RootUrl": "string"
     }
   },
+  "auth": {
+    "teamsplitter32ff3d29": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
   "function": {
     "TeamSplitterAPI": {
       "Arn": "string",
